1f147ff83f3d8315ece3bcc4ba0d8054de8936c3
Jose Cordero Fuentealba <jocorderf@gmail.com>
2020-08-19 11:51:00 -0400

    added control flow and final comment

--- daffy_duck.rb
+++ daffy_duck.rb
@@ -2,3 +2,10 @@ puts "Please enter your text"
 user_input = gets.chomp
 
 user_input.downcase! #This will allow us to only look for "s" and not "S".
+
+if user_input.include? "s"
+    user_input.gsub!(/s/, "th") #This replaces the s in the string for th.
+    puts "Daffy Duck: #{user_input}" #This prints the new string
+else
+    puts user_input
+end
